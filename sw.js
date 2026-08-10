7f1ffe84ae765b0c5fd39c130b155fbb519a4786
cesardenize89-cmd <cesardenize89@gmail.com>
2026-08-10 15:34:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lectio-v18';
+const CACHE='lectio-v19';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./logo_mark.svg','./logo_horizontal.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
